84ff870f7065c9abe5b5affb5c5bedb286e507cb
parastelios <parastelios@hotmail.com>
2018-11-17 18:22:54 +0100

    Added functions for weather data

---
+++ jusipy/latlong_features/cru.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+class CRU(object):
+    def __init__(self):
+        '''
+        Loading the weather datasets
+        Data from Center of Enviromental Data Analysis, data mean values between 2011-2016
+        Data link: http://data.ceda.ac.uk/badc/cru/data/cru_ts/cru_ts_4.01/data/
+        '''        
+        # loads pre, cld, temp
+        self._missingvalue = -999
+        pre =  np.load(open('jusipy/latlong_features/data/weather_data/pre.npy', 'rb'))
+        cld =  np.load(open('jusipy/latlong_features/data/weather_data/cld.npy', 'rb')) 
+        tmp =  np.load(open('jusipy/latlong_features/data/weather_data/tmp.npy', 'rb'))
+        pet =  np.load(open('jusipy/latlong_features/data/weather_data/pet.npy', 'rb'))
+        self._data = { 'rain' :pre, #  precipitation: mm/month
+                       'cloud':cld, #  cloud cover: percentage
+                       'temp':tmp,  #  near-surface temperature: degrees Celsius
+                       'pet':pet    #  potential evapotranspiration:  mm/day
+                     }
+        
+    #edef
+        
+    def lookup_single(self, feature, lat, long, pixel_window=0):
+        '''
+        Look for lat and long and return the mean values for the pixel window
+        '''
+        v = latlong_lookup(self._data[feature], lat, long, pixel_window)
+        v = v[v!=self._missingvalue]
+        
+        if len(v) == 0:
+            return 0 # we don't query over water
+        #fi
+        return np.mean(v)
+    #edef
+    
+    def lookup(self, lat, long, pixel_window=0):
+        """
+        
+        """        
+        return np.array([ self.lookup_single(feat, lat, long, pixel_window) for feat in self._data ])
+    #edef
+    
+    def get(self, coords, pixel_window=0):
+        '''
+        
+        '''
+        D = np.array([self.lookup(lat, long, pixel_window=pixel_window) for (lat,long) in coords])
+        return pd.DataFrame(D, columns=list(self._data.keys()))
+    #edef
